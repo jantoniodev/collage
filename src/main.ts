04c9e7b92f5af7a172b9faeec2017be5ccae88a0
jantoniodev <jose.lopezh95@gmail.com>
2023-08-18 23:13:19 -0400

    Agrega función para cambiar resolución

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import backgroundRemoval from '@imgly/background-removal'
 
 const FRAME_WIDTH = 40
 
-const ratio = 0.7
-const canvasWidth = 1440 * ratio
-const canvasHeight = 900 * ratio
+const resolution = {
+    width: 1440,
+    height: 900,
+}
+
+const zoom = 0.7
+
+const getResolution = () => {
+    return {
+        width: resolution.width * zoom,
+        height: resolution.height * zoom,
+    }
+}
 
 const stage = new Konva.Stage({
     container: 'container',
@@ -14,17 +24,15 @@ const stage = new Konva.Stage({
 })
 
 const layer = new Konva.Layer({
-    x: (stage.width() / 2) - (canvasWidth / 2),
-    y: (stage.height() / 2) - (canvasHeight / 2),
-    width: canvasWidth,
-    height: canvasHeight,
+    x: (stage.width() / 2) - (getResolution().width / 2),
+    y: (stage.height() / 2) - (getResolution().height / 2),
 })
 const transformer = new Konva.Transformer()
 const background = new Konva.Rect({
     x: 0,
     y: 0,
-    width: canvasWidth,
-    height: canvasHeight,
+    width: getResolution().width,
+    height: getResolution().height,
     fill: '#8598A9',
     listening: false,
 })
@@ -44,21 +52,28 @@ const uploadPhotoButton = document.getElementById('upload-fotos')
 const uploadObjectButton = document.getElementById('upload-object')
 const uploadObjectProgress = document.getElementById('upload-object-progress')
 const downloadButton = document.getElementById('download-button')
+const resolutionSelect = document.getElementById('resolution-select')
+
+resolutionSelect?.addEventListener('change', (event) => {
+    const selectElement = event.target as HTMLSelectElement
+    const [selectedWidth, selectedHeight] = selectElement.value.split('x').map(e => parseInt(e))
+    resizeWorkspace(selectedWidth, selectedHeight)
+})
 
 downloadButton?.addEventListener('click', () => {
     const imageDataUrl = stage.toDataURL({
         mimeType: 'image/jpeg',
         x: layer.x(),
         y: layer.y(),
-        width: canvasWidth,
-        height: canvasHeight,
+        width: background.width(),
+        height: background.height(),
         quality: 1,
         /*
             La expresión para pixel ratio se puede simplificar matemáticamente como 1 / ratio
             pero al hacerlo así existe un problema de redondeo en la resolución final con la que se exporta la imágen
             por lo tanto se deja de la forma completa.
         */
-        pixelRatio: (canvasWidth / ratio) / canvasWidth,
+        pixelRatio: (background.width() / zoom) / background.width(),
     })
 
     const link = document.createElement('a')
@@ -178,4 +193,15 @@ const handleProgress = (key: string, current: number, total: number) => {
 
 const handleImageClick = (image: Konva.Group) => {
     transformer.setNodes([image])
+}
+
+const resizeWorkspace = (width: number, height: number) => {
+    resolution.width = width,
+    resolution.height = height
+
+    layer.x((stage.width() / 2) - (getResolution().width / 2))
+    layer.y((stage.height() / 2) - (getResolution().height / 2))
+
+    background.width(getResolution().width)
+    background.height(getResolution().height)
 }
